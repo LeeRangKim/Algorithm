d62b604e55aad67c6d223a4ebda208826da0496b
LeerangKim <97589941+LeeRangKim@users.noreply.github.com>
2022-09-09 10:29:48 +0900

    [Bronze IV] Title: 기찍 N, Time: 1200 ms, Memory: 32016 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for(int i = N ; 0 < i; i--){
+            System.out.println(i);
+        }
+
+    }
+}
